5c79417acb08889c71b8ebd7928a1a2a51d67ad7
Moazzam Khan <moazzam.khan.wa@gmail.com>
2020-02-06 21:39:51 +0500

    Added comments

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 function App() {
 
+  console.log('React')
   return (
     <div className="App">
       <img src="https://cdn.freebiesupply.com/logos/thumbs/2x/spacex-logo.png" style={{ width: 300, display: 'block', margin: 'auto' }}></img>
